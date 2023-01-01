46b38f0ba818ad340b7337607c2b6c42effdc172
Ehsanba23 <ebashiri33@gmail.com>
2023-01-01 14:38:23 +0330

    Add project files.

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please Enter h:");
+            double h = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Please Enter v:");
+            double v = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Masaht is={0}", 0.5 * h * v);
+
+        }
+    }
+}
